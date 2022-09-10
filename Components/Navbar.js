61d480d33697ab9d8ef6f125e4e51b4cf436ffef
Alirezanoun <alirezanoun14@gmail.com>
2022-09-10 19:50:13 -0400

    navigation bar

---
+++ Components/Navbar.js
@@ -0,0 +1,21 @@
+const Navbar = `
+    <nav class="navbar">
+      <div class="navbar__container">
+        <a href="#home" id="navbar__logo2"><img src="./img/logo.png" width="55px" height="50px" /></a>
+        <a href="#home" id="navbar__logo" class="font1">Zahra Nouhehkhan</a>
+        <div class="navbar__toggle" id="mobile-menu">
+          <span class="bar"></span> <span class="bar"></span>
+          <span class="bar"></span>
+        </div>
+        <ul class="navbar__menu">
+          <a href="https://www.instagram.com/zgraphicals/" target="_blank">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <li class="navbar__btn">
+            <a href="#sign-up" class="button" id="signup">Contact Me</a>
+          </li>
+        </ul>
+      </div>
+    </nav>`;
+
+export default Navbar;
---
+++ index.js
@@ -0,0 +1,6 @@
+import InjectUI from './JavaScript/injectUI.js';
+import Navbar from './Components/Navbar.js';
+
+window.addEventListener('DOMContentLoaded', () => {
+	InjectUI(Navbar, 'navigation-bar');
+});
