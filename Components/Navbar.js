0490f10e600d146f398530ae77089b9d3908af82
Alirezanoun <alirezanoun14@gmail.com>
2022-09-10 20:31:23 -0400

    updated nav and footer

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = `
             <i class="fab fa-instagram"></i>
           </a>
           <li class="navbar__btn">
-            <a href="#sign-up" class="button" id="signup">Contact Me</a>
+            <a href="#sign-up" class="button" id="signup">Contact Us</a>
           </li>
         </ul>
       </div>
--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import InjectUI from './JavaScript/injectUI.js';
 import ScrollAnimation from './JavaScript/ScrollAnimation.js';
 import MobileMenu from './JavaScript/MobileMenu.js';
 import Navbar from './Components/Navbar.js';
+import Footer from './Components/Footer.js';
 
 window.addEventListener('DOMContentLoaded', () => {
 	InjectUI(Navbar, 'navigation-bar');
+	InjectUI(Footer, 'footer-copyright');
 	MobileMenu();
 });
 
