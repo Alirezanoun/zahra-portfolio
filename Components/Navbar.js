591875c32950c53a47e1698c126e7b52637fb8ec
Alirezanoun <alirezanoun14@gmail.com>
2022-09-11 19:47:56 -0400

    removed header navbar & updated about me

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -2,7 +2,6 @@ const Navbar = `
     <nav class="navbar">
       <div class="navbar__container">
         <a href="./index.html" id="navbar__logo2"><img src="./img/logo.png" width="55px" height="50px" /></a>
-        <a href="./index.html" id="navbar__logo" class="font1">Zahra Nouhehkhan</a>
         <div class="navbar__toggle" id="mobile-menu">
           <span class="bar"></span> <span class="bar"></span>
           <span class="bar"></span>
