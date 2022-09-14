44180d33eb413acbbe6386f84f7bf7105f8ce753
Alirezanoun <alirezanoun14@gmail.com>
2022-09-14 11:17:40 -0400

    added contanct page and removed the footer contact info

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,21 +1,4 @@
 const Footer = `
-<div class="main" id="">
-	<div class="main__container">
-		<div class="main__content">
-			<div class="container reveal">
-				<div class="text-card">
-					<h2 class="fix2">Email</h2>
-					<h1 class="fix1">zahranouhehkhan@gmail.com</h1>
-					<button class="main__btn"><a href="mailto:zahranouhehkhan@gmail.com" target="_blank">Email Me</a></button>
-				</div>
-				<div class="main__img--container">
-					<div class="main__img--card" id="card-2"></div>
-				</div>
-			</div>
-		</div>
-	</div> 
-</div>
-
 <div class="footer__container">
 	<img src="./img/Waves2.png" class="waves2" />
 	<p class="website__rights" id="sign-up">© ZAHRA NOUHEHKHAN 2022</p>
