337dc110ea9d713247c5622a069cf2e0f044f78f
Alirezanoun <alirezanoun14@gmail.com>
2022-09-26 21:34:03 -0400

    removed the dark mode stuff

--- index.js
+++ index.js
@@ -11,20 +11,3 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 window.addEventListener('scroll', ScrollAnimation);
-
-const options = {
-	bottom: '64px', // default: '32px'
-	right: 'unset', // default: '32px'
-	left: '32px', // default: 'unset'
-	time: '0.5s', // default: '0.3s'
-	mixColor: '#fff', // default: '#fff'
-	backgroundColor: '#fff', // default: '#fff'
-	buttonColorDark: '#100f2c', // default: '#100f2c'
-	buttonColorLight: '#fff', // default: '#fff'
-	saveInCookies: true, // default: true,
-	label: '<img src="./img/EDCEE156-F488-4FB5-96C5-8FC4B4BDFF53.png" width="30px" />', // default: ''
-	autoMatchOsTheme: true, // default: true
-};
-
-const darkmode = new Darkmode(options);
-darkmode.showWidget();
