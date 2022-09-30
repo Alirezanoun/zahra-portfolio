86a0745da69fa3f169bb95973c34a333e16b1c20
Alirezanoun <alirezanoun14@gmail.com>
2022-09-30 18:15:22 -0400

    added new text to navbar

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -2,6 +2,7 @@ const Navbar = `
     <nav class="navbar">
       <div class="navbar__container">
         <a href="./index.html" id="navbar__logo2"><img src="./img/logo.png" width="55px" height="50px" /></a>
+        <h1 class="expanded-text">ZAHRA NOUHEHKHAN</h1>
         <div class="navbar__toggle" id="mobile-menu">
           <span class="bar"></span> <span class="bar"></span>
           <span class="bar"></span>
