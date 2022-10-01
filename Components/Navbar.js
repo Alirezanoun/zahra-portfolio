dd407d16c2a1be600f6d1d29649f00fcc8a429bd
Alirezanoun <alirezanoun14@gmail.com>
2022-10-01 19:34:05 -0400

    removed name from navbar

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -2,7 +2,6 @@ const Navbar = `
     <nav class="navbar">
       <div class="navbar__container">
         <a href="./index.html" id="navbar__logo2"><img src="" width="55px" height="50px" /></a>
-        <h1 class="expanded-text">ZAHRA NOUHEHKHAN</h1>
         <div class="navbar__toggle" id="mobile-menu">
           <span class="bar"></span> <span class="bar"></span>
           <span class="bar"></span>
