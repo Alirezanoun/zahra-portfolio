88cc416024a8147348a7a056db4706e459b0d1f0
Alirezanoun <alirezanoun14@gmail.com>
2022-10-02 15:42:26 -0400

    added animation to text

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,7 +1,7 @@
 const Navbar = `
     <nav class="navbar">
       <div class="navbar__container">
-        <a href="./index.html" id="navbar__logo2"><img src="" width="55px" height="50px" /></a>
+        <a href="#home" id="navbar__logo2"><img src="" width="55px" height="50px" /></a>
         <div class="navbar__toggle" id="mobile-menu">
           <span class="bar"></span> <span class="bar"></span>
           <span class="bar"></span>
