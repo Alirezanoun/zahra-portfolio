010b55724c390fff109aa5cd57cfda2db38d3b57
Alirezanoun <alirezanoun14@gmail.com>
2022-10-02 17:46:57 -0400

    tried fixing the underline

--- JavaScript/MobileMenu.js
+++ JavaScript/MobileMenu.js
@@ -1,11 +1,10 @@
 function MobileMenu() {
 	const menu = document.querySelector('#mobile-menu');
 	const menuLinks = document.querySelector('.navbar__menu');
-	const navLogo = document.querySelector('#navbar__logo');
+	const navLogo = document.querySelector('#navbar__logo2');
 	const elem = document.querySelector('.highlight');
-	const homeMenu = document.querySelector('#home-page');
 	const aboutMenu = document.querySelector('#about-page');
-	const servicesMenu = document.querySelector('#services-page');
+	const servicesMenu = document.querySelector('#work-page');
 
 	// Display Mobile Menu
 	const mobileMenu = () => {
@@ -22,12 +21,11 @@ function MobileMenu() {
 
 		// adds 'highlight' class to my menu items
 		if (window.innerWidth > 960 && scrollPos < 600) {
-			homeMenu.classList.add('highlight');
-			aboutMenu.classList.remove('highlight');
+			aboutMenu.classList.add('highlight');
+			servicesMenu.classList.remove('highlight');
 			return;
 		} else if (window.innerWidth > 960 && scrollPos < 1400) {
 			aboutMenu.classList.add('highlight');
-			homeMenu.classList.remove('highlight');
 			servicesMenu.classList.remove('highlight');
 			return;
 		} else if (window.innerWidth > 960 && scrollPos < 2345) {
