b123d6350c7dad96c95a974d5dfa3b2b3f034980
Alirezanoun <alirezanoun14@gmail.com>
2022-10-08 10:22:56 -0400

    fixed navbar

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -1,23 +1,25 @@
 const Navbar = `
     <nav class="navbar">
       <div class="navbar__container">
-        <a href="./index.html" id="navbar__logo2"><img src="" width="55px" height="50px" /></a>
+        <a href="./index.html" id="navbar__logo2"></a>
         <div class="navbar__toggle" id="mobile-menu">
           <span class="bar"></span> <span class="bar"></span>
           <span class="bar"></span>
         </div>
-        <ul class="navbar__menu">
+        <ul class="right-side">
          <li class="navbar__item">
-            <a href="#about" class="navbar__links" id="about-page">ABOUT ME</a>
+            <a href="#about" class="navbar__links" id="about-page">ABOUT</a>
           </li>
           <li class="navbar__item">
-            <a href="#work" class="navbar__links" id="work-page">DESIGNS</a>
+            <a href="#work" class="navbar__links" id="work-page">WORK</a>
           </li>
+          </ul>
+          <ul class="left-side">
           <li class="navbar__item">
             <a href="https://www.instagram.com/zgraphicals/" target="_blank" class="navbar__links" id="insta">INSTAGRAM</a>
           </li>
-                    <li class="navbar__item">
-            <a href="./contact.html" class="navbar__links" id="work-page">CONTACT ME</a>
+          <li class="navbar__item">
+            <a href="./contact.html" class="navbar__links" id="work-page">CONTACT</a>
           </li>
         </ul>
       </div>
